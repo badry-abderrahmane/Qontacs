debad8c4157a344f6195d2738da7af9383039cfe
Badry Abderrahmane <badry-abderrahmane@git.com>
2019-01-19 21:48:33 +0000

    Added Needed Global Routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,42 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+/**
+ * Welcome View Route
+ */
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/**
+ * Contacts Routes
+ */
+Route::prefix('contacts')->group(function () {
+    
+    /**
+     * Contacts List Route
+     */
+    Route::get('/', 'ContactsController@index')->name('contacts.index');
+    
+    /**
+     * CRUD Methods Routes
+     */
+    Route::post('/store', 'ContactsController@store')->name('contacts.store');
+    Route::put('/update', 'ContactsController@update')->name('contacts.update');
+    Route::delete('/destroy/{id}', 'ContactsController@destroy')->name('contacts.destroy');
+
+    /**
+     * Specific Route For Datatables Response
+     */
+    Route::get('/all/paginated', 'ContactsController@getAllPaginated')->name('contacts.paginated');
+    
+    /**
+     * CRUD Views Routes
+     */
+    Route::get('/create', 'ContactsController@create')->name('contacts.create');
+    Route::get('/show/{id}', 'ContactsController@show')->name('contacts.show');
+    Route::get('/edit/{id}', 'ContactsController@edit')->name('contacts.edit');
+    Route::get('/delete/{id}', 'ContactsController@delete')->name('contacts.delete');
+    
 });
