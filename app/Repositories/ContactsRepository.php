98335382000c54f1c14428b6ffdc1760edddc7e7
Badry Abderrahmane <badry-abderrahmane@git.com>
2019-01-19 22:00:59 +0000

    Added Contact Controller + Repository + Resource

---
+++ app/Repositories/ContactsRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories;
+
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Http\Resources\ContactResource;
+use App\Models\Contact;
+
+class ContactsRepository
+{
+    
+    
+}
+
