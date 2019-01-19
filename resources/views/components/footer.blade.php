56da9b95969bdbc237e72c14d8970586d6bc19d8
Badry Abderrahmane <badry-abderrahmane@git.com>
2019-01-19 21:45:21 +0000

    Added Layout Components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<footer id="footer" class="bg-dark text-light py-3">
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-6 mb-2 text-right">
+                <ul class="nav float-right">
+                    <li><a class="text-white mr" rel="nofollow" href="https://github.com/ThemesGuide/bootstrap-themes" title="Follow on GitHub"><i class="h1 fa fa-fw fa-facebook fa-2x ion-logo-github"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer> 
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary" id="navbar1">
+    <div class="container">
+        <a class="navbar-brand mr-1 mb-1 mt-0" href="../">Qontacs</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse justify-content-center" id="collapsingNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link">
+                        Beta
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav> 
